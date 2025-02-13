a011e0fc2e025b5e3ead6ea7a95eb526ada100e6
Pavel Podrazký <podrazkypavel002@gmail.com>
2025-02-13 18:02:05 +0100

    created base

--- app/views/cesty.py
+++ app/views/cesty.py
@@ -1,8 +1,16 @@
+import datetime
+
 from flask import render_template
 
 from app import app
 
 
-@app.route("/test")
-def test():
+@app.route("/")
+def index():
+    year = datetime.datetime.now().year
+    return render_template("index.html", year=year)
+
+
+@app.route("/about")
+def about():
     return render_template("base.html")
