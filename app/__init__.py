4a34cf8755469749b281f2303fe21365f64a5472
Pavel Podrazký <podrazkypavel002@gmail.com>
2025-02-13 16:47:17 +0100

    renamed api dir to app dir

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from api.views import cesty
+from app.views import cesty
--- app/views/cesty.py
+++ app/views/cesty.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from api import app
+from app import app
 
 
 @app.route("/test")
