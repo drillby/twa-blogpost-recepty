c5588681ed508f23510c8c115e707044e8ed0879
Marek Šimon <mareksimon34@gmail.com>
2025-03-01 15:42:20 +0100

    modified logic for adding recipes

--- app/views/recipe_routes.py
+++ app/views/recipe_routes.py
@@ -2,14 +2,10 @@ import vercel_blob
 from flask import redirect, render_template, request, url_for, flash
 from werkzeug.utils import secure_filename
 
-import os
-
-
-
 from app import app, db
 
 from ..models.models import Recipe, RecipeImage, User, UserLikedRecipes
-
+from io import BytesIO
 
 @app.route("/recipe/<int:id>")
 def recipe_detail(id):
@@ -22,16 +18,16 @@ def recipe_detail(id):
 def add_recipe():
     if request.method == "POST":
         # logika pro pridani receptu
-        recipe_name = request.form.get('recipeName')
-        ingredients = request.form.get('ingredients')
-        steps = request.form.get('steps')
-        category = request.form.get('category')
-        pictures = request.files.getlist('pictures')
+        recipe_name = request.form.get("recipeName")
+        ingredients = request.form.get("ingredients")
+        steps = request.form.get("steps")
+        category = request.form.get("category")
+        pictures = request.files.getlist("pictures")
 
         # Validace vstupů
-        if not recipe_name or not ingredients or not steps or not category:
-            flash('Všechna pole musí být vyplněná!', 'danger')
-            return redirect(url_for('add_recipe'))
+        if not recipe_name or not ingredients or not steps or not category or not pictures:
+            flash("Všechna pole musí být vyplněná!", "danger")
+            return redirect(url_for("add_recipe"))
 
         # Přiřazení statického ID uživatele
         user_id = 11  # Statické ID uživatele
@@ -43,28 +39,19 @@ def add_recipe():
 
         # Uložení obrázků
         for index, picture in enumerate(pictures):
-            if picture and allowed_file(picture.filename):
-                filename = secure_filename(picture.filename)
-                file_extension = filename.rsplit('.', 1)[1].lower()
-                image_path = f"recipe-images/{new_recipe.id}-{index + 1}.{file_extension}"
+             if picture:
+                img_data = BytesIO()
+                picture.save(img_data)
+                img_bytes = img_data.getvalue()
+                file_extension = picture.filename.split(".")[-1]
+                image_res = vercel_blob.put(f"recipe-images/{new_recipe.id}-{index}.{file_extension}", img_bytes)
+                image_url = image_res["url"]
                 
-                # Nahrání obrázku na Vercel Blob Storage
-                img_bytes = picture.read()
-                profile_picture_res = vercel_blob.put(image_path, img_bytes)
-                profile_picture_url = profile_picture_res["url"]
-                
-                new_image = RecipeImage(recipe_id=new_recipe.id, image_url=profile_picture_url)
-                db.session.add(new_image)
-            else:
-                flash('Nepovolený formát souboru!', 'danger')
-                return redirect(url_for('add_recipe'))
-
-        db.session.commit()
-        flash('Recept byl úspěšně přidán!', 'success')
-        return redirect(url_for('recipe_detail', id=new_recipe.id))
-    else:
-        return render_template("add-recipe.html")
-
-def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+                recipe_image = RecipeImage(recipe_id=new_recipe.id, image_url=image_url)
+                db.session.add(recipe_image)
+                db.session.commit()
+        
+        flash("Recept byl úspěšně přidán.", "success")
+        return redirect(url_for("recipe_detail", id=new_recipe.id))
+    
+    return render_template("add-recipe.html")
