cd7b5f57f6a40db92b3059408e7192d8696aae1c
jankral159 <xkraj146@studenti.czu.cz>
2025-03-02 22:21:27 +0100

    upraveno related_recipes a názvy ve featured-card

--- app/views/recipe_routes.py
+++ app/views/recipe_routes.py
@@ -9,6 +9,9 @@ from ..models.models import Recipe, RecipeImage, User, UserLikedRecipes
 @app.route("/recipe/<int:id>")
 def recipe_detail(id):
     recipe = db.session.query(Recipe).get_or_404(id)
+
+    related_recipes = Recipe.query.filter(Recipe.tag == recipe.tag, Recipe.id != recipe.id).limit(4).all()
+    
     ##recipe = Recipe.query.get_or_404(id)
     ##upravit related_recipes_id
     recipe_data = {"title": recipe.title,
@@ -18,11 +21,16 @@ def recipe_detail(id):
         "instructions": recipe.instructions,
         "ingredients": recipe.ingredients.split("\n"),  # Rozdělit ingredience na řádky
         "images": [img.image_url for img in recipe.images],
-        "related_recipes_id": 22
+        ##"related_recipes_id": [22],
         ##    related.id for related in db.session.query(Recipe).filter(Recipe.tag == recipe.tag).limit(4)
-        ##"related_recipes_id": [
-        ##    related.id for related in Recipe.query.filter(Recipe.tag == recipe.tag).limit(4)
-        ##]
+        "related_recipes_id": [
+                     {
+                "id": r.id,
+                "title": r.title,
+                "image": r.images[0].image_url if r.images else None,
+            }
+            for r in related_recipes
+        ],
         }
     return render_template("recipe-detail.html", recipe=recipe_data)
 
