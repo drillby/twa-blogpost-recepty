9bc46d7eb08f90a61c59c010f3cf32297f4d0496
OndrejSt <you@example.com>
2025-03-05 20:20:08 +0100

    Landing page + searching

--- app/views/search_routes.py
+++ app/views/search_routes.py
@@ -8,33 +8,101 @@ from app import app, db
 
 from ..models.models import Recipe, RecipeImage, User, UserLikedRecipes
 
+from fuzzywuzzy import fuzz
+
+from sqlalchemy.sql.expression import func, desc
+
 
 @app.route("/search")
 def search():
-    query = request.args.get("query")
+    query = request.args.get("query").lower()
+    
+    #prázdné query vrátí na domácí stránku
     if not query:
         return redirect(url_for("index"))
-    res = []
-    return render_template("search-results.html", res=res, query=query)
+    
+
+    # sorting by tags
+    tag_synonyms = {
+        "vecere": ["večeře", "večeře recepty", "recepty na večeři", "co k večeři", "večerní jídla", "jídla k večeři", "večeře pro rodinu", "rychlá večeře", "večerní menu", "večeře pro dvě", "večeře pro děti", "co připravit na večeři"],
+        "snidane": ["snídaně", "snídaně recepty", "co na snídani", "rychlá snídaně", "snídaně pro děti", "snídaně pro dva", "ideální snídaně", "sladká snídaně", "slaná snídaně", "snídaně do postele", "snídaně na víkend"],
+        "dezerty": ["dezert", "dezerty recepty", "sladkosti", "koláče", "dorty", "pečení", "cukroví", "sladké jídlo", "sladké recepty", "koláče a dezerty", "vánoční cukroví", "sladké dobroty", "dort na oslavu"],
+        "hlavni_jidla": ["hlavní jídla", "hlavní chod", "hlavní pokrm", "co na oběd", "oběd", "jídlo k obědu", "jednoduchá hlavní jídla", "hlavní jídlo pro rodinu", "jídlo pro dva", "ráno k obědu", "dobrá hlavní jídla", "oběd pro děti"],
+        "napoje": ["nápoje", "pití", "nápoje recepty", "co na pití", "lahodné nápoje", "zdravé nápoje", "nealkoholické nápoje", "smoothie", "cocktaily", "ovocné nápoje", "studené nápoje", "teplé nápoje", "nápoje pro děti"],
+        "polevky": ["polévky", "polévka", "polévka recepty", "zeleninová polévka", "masová polévka", "rychlá polévka", "domácí polévky", "krémové polévky", "česnečka", "vývary", "čočková polévka", "zeleninový vývar", "pohanková polévka"],
+        "predkrmy": ["předkrmy", "předkrm", "snídaňové předkrmy", "předkrmy recepty", "studené předkrmy", "teplé předkrmy", "ideální předkrm", "sýrové předkrmy", "předkrmy pro hosty", "lahodné předkrmy", "malé jídlo", "co na předkrm"],
+        "svaciny": ["svačiny", "svačina", "co na svačinu", "svačiny pro děti", "svačiny pro dospělé", "rychlé svačiny", "svačiny do školy", "svačiny pro školáky", "svačina pro sportovce", "malé jídlo", "sladká svačina", "slaná svačina", "svačiny do práce"]
+    }
+        
+    for tag, synonyms in tag_synonyms.items():
+        if any(synonym in query for synonym in synonyms):
+            recipes_tag = Recipe.query.filter(Recipe.tag == tag).all()
+            return render_template("search-results.html", res=recipes_tag, query=query)
+
+
+    #sorting by title or ingredients
+    recipes = Recipe.query.all()
+
+    matches = []
+
+    for recipe in recipes:
+        match_ratio_title = fuzz.token_sort_ratio(recipe.title.lower(), query)
+        match_ratio_ingredients = fuzz.token_sort_ratio(recipe.title.lower(), query)
+        
+        if match_ratio_title > 60 or match_ratio_ingredients > 70: 
+            weighted_match_ratio = 0.65 * match_ratio_title + 0.35 * match_ratio_ingredients 
+            matches.append(recipe, weighted_match_ratio)
+                   
+    matches.sort(key=lambda x: x[1], reverse=True)
+    sorted_recipes = [match[0] for match in matches]
+    
+    return render_template("search-results.html", res=sorted_recipes, query=query)
 
 
 @app.route("/")
 def index():
-    year = datetime.datetime.now().year
 
-    recipes = []
+    #Featured selection
+    time_param = request.args.get("time")
+    
+    if time_param == "NaN" or time_param is None:
+        print("Nepodařilo se získat čas od klienta, používám čas serveru.xD")
+        client_time = datetime.datetime.now().hour
+    else:
+        try:
+            client_time = int(time_param)
+            print(client_time)
+        except ValueError:
+            print("Neplatná hodnota času, používám serverový čas.")
+            client_time = datetime.datetime.now().hour
 
-    recipes_featured = []
+    searching_tag = ""
 
-    return render_template(
-        "index.html", year=year, recipes=recipes, recipes_featured=recipes_featured
-    )
+    if 0 < client_time < 9:
+        searching_tag = "snidane"
+    elif client_time < 11:
+        searching_tag = "svacina"
+    elif client_time < 13:
+        searching_tag = "hlavni_jidla"
+    elif client_time < 15:
+        searching_tag = "dezerty"
+    elif client_time < 17:
+        searching_tag = "svacina"   
+    else:
+        searching_tag = "vecere"                           
+                    
+    recipes_featured = Recipe.query.filter(Recipe.tag == searching_tag).order_by(func.random()).limit(4).all()
+               
+     #Newest selection               
+
+    recipes_newest = Recipe.query.order_by(desc(Recipe.created_at)).limit(10).all()
+
+    return render_template("index.html", recipes=recipes_newest, recipes_featured=recipes_featured)
 
 
 @app.route("/generate-test-data")
 def generate_test_data():
-    if app.config["TESTING"] == "False":
-        return "Testovací data mohou být generována pouze v testovacím prostředí"
+    
 
     user = request.args.get("user", 0, type=int)
     recipe = request.args.get("recipe", 0, type=int)
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 
 if __name__ == "__main__":
     with app.app_context():
-        app.run()
+        app.run(debug=True)
